a5671f2e7aaa802bbf3f7922a69e8095f929d2ad
iichhhaaa <142071811+iichhhaaa@users.noreply.github.com>
2025-10-10 07:02:39 +0700

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,27 +1,37 @@
-# MLProject/upload_to_gdrive.py
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 import os
 import json
 
-def upload_to_gdrive():
+def upload_to_drive():
     creds_json = os.getenv("GDRIVE_CREDENTIALS")
     folder_id = os.getenv("GDRIVE_FOLDER_ID")
     if not creds_json or not folder_id:
-        raise ValueError("Google Drive credentials or folder ID not set!")
+        raise Exception("Missing GDRIVE_CREDENTIALS or GDRIVE_FOLDER_ID")
 
     creds_dict = json.loads(creds_json)
-    creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=["https://www.googleapis.com/auth/drive.file"])
+    creds = service_account.Credentials.from_service_account_info(
+        creds_dict,
+        scopes=["https://www.googleapis.com/auth/drive"]
+    )
+
     service = build("drive", "v3", credentials=creds)
 
-    for root, dirs, files in os.walk("MLProject/mlruns"):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            file_metadata = {"name": file_name, "parents": [folder_id]}
-            media = MediaFileUpload(file_path, resumable=True)
-            uploaded = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
-            print(f"Uploaded {file_name} (ID: {uploaded.get('id')})")
+    # Upload file mlruns.zip (hasil training)
+    file_name = "mlruns.zip"
+    if not os.path.exists(file_name):
+        os.system(f"zip -r {file_name} mlruns")
+
+    file_metadata = {"name": file_name, "parents": [folder_id]}
+    media = MediaFileUpload(file_name, mimetype="application/zip")
+
+    service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields="id"
+    ).execute()
+    print("✅ Upload success to Google Drive!")
 
 if __name__ == "__main__":
-    upload_to_gdrive()
+    upload_to_drive()
